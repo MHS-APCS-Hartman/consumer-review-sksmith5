853d72d62bc9150df912af738f260e00c42c47b7
sksmith5 <59923194+sksmith5@users.noreply.github.com>
2020-01-17 09:24:45 -0500

    Update Review.java
    Adding in the total sentiment code and star rating

--- Review.java
+++ Review.java
@@ -1,3 +1,7 @@
+//1. It starts off with you grabbing the file name then it starts to break it down getting rid of the punctuation, then it will take that new string and find the sentiment value of it. It will take that output and add it to the totalVal where it will print the total sentiment value.
+//2. a) The ratings don't make sense, since the method won't play out, but if it did it seems to make sense, since the review coorelate to positive or negative sentimental value due to it's either good or harsh words. 
+//2. b) I feel coding in actual words that give more sensual values and have more of variety of words that could be good and bad could narrow down to the specifics of the rating.
+//3. His code is wrong due to the fact if it's less than 0 it would go through the entire problem and it would spit out "4321" and that doesn't give the user the correct review it needs so all he needs to do to create correct code is add two statements of </> connected with an && symbol so the whole problem would make sure both statements are true before executing.
 import java.util.Scanner;
 import java.io.File;
 import java.util.HashMap;
@@ -80,9 +84,83 @@ public class Review {
       System.out.println("Unable to locate " + fileName);
     }
     //make sure to remove any additional space that may have been added at the end of the string.
+    //totalSentiment("SimpleReview.txt");
     return temp.trim();
   }
+     /**
+   * Returns the word after removing any beginning or ending punctuation
+   */
+  public static String removePunctuation( String word )
+  {
+    while(word.length() > 0 && !Character.isAlphabetic(word.charAt(0)))
+    {
+      word = word.substring(1);
+    }
+    while(word.length() > 0 && !Character.isAlphabetic(word.charAt(word.length()-1)))
+    {
+      word = word.substring(0, word.length()-1);
+    }
+    
+    return word;
+  }
+
   
+  public static double totalSentiment(String fileName)
+  {
+   //open file
+   //pick each word -> when I find a space, all the stuff before was a word. But I've got to remove punctuation.
+   //look up sentiment value
+   //add to total
+   //return value
+      String file = textToString(fileName);
+      String word = "";
+      String space = " ";
+      double totalVal = 0;
+      for(int i = 0; i < file.length(); i++)
+      {
+         removePunctuation(fileName);
+         String str = file.substring(i, i+1);
+         if(str.equals(space))
+         {
+            totalVal += sentimentVal(word);
+            word = "";
+         }
+         else
+         {
+            word += str;
+         }
+      }
+      return totalVal;
+      
+      
+  }
+  
+  public static int starRating(String fileName);
+  {
+      String rate = totalSentiment(fileName);
+         if(rate > 10)
+         {
+            return 4;
+         }
+         else if(rate < 10 && rate > 5)
+         {
+            return 3;
+         }
+         else if(rate < 5 && rate > 0)
+         {
+            return 2;
+         }
+         else if(rate <= 0)
+         {
+            return 1;
+         }
+         else
+         {
+            return 0;
+         }
+      
+  }
+ 
   /**
    * @returns the sentiment value of word as a number between -1 (very negative) to 1 (very positive sentiment) 
    */
@@ -114,26 +192,6 @@ public class Review {
     return punc;
   }
   
-   /**
-   * Returns the word after removing any beginning or ending punctuation
-   */
-  public static String removePunctuation( String word )
-  {
-    while(word.length() > 0 && !Character.isAlphabetic(word.charAt(0)))
-    {
-      word = word.substring(1);
-    }
-    while(word.length() > 0 && !Character.isAlphabetic(word.charAt(word.length()-1)))
-    {
-      word = word.substring(0, word.length()-1);
-    }
-    
-    return word;
-  }
- 
-
-
-  
   /** 
    * Randomly picks a positive adjective from the positiveAdjectives.txt file and returns it.
    */
@@ -164,5 +222,7 @@ public class Review {
     } else {
       return randomNegativeAdj();
     }
+    
+    
   }
 }
